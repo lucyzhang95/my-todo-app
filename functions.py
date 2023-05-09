9941ed5d483d059a16f3df9972fdc68524c91aa0
Lucy Zhang <bazhang@scripps.edu>
2023-05-09 16:56:28 -0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+FILEPATH = "/Users/lucyzhang1116/Documents/Udemy_PythonCourse/D1-D19/Day18_19/todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+# To behave as a procedure
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print(get_todos())
